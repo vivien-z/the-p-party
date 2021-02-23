689da19cb262c4b70e323eede0382b2561207163
elenicehalma <elenice.halma@gmail.com>
2021-02-23 11:38:57 -0500

    Review model created.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_162625) do
+ActiveRecord::Schema.define(version: 2021_02_23_163613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_02_23_162625) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["theme_id"], name: "index_bookings_on_theme_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "themes", force: :cascade do |t|
@@ -53,5 +61,6 @@ ActiveRecord::Schema.define(version: 2021_02_23_162625) do
 
   add_foreign_key "bookings", "themes"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bookings"
   add_foreign_key "themes", "users"
 end
