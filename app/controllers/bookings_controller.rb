a4472cf917ad5710e735c93883a1cf969ff75fde
elenicehalma <elenice.halma@gmail.com>
2021-02-23 14:08:17 -0500

    Creating booking without path.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+
+    if booking.save
+      redirect_to @booking, notice: "Booking was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:starting_date, :ending_date, :address)
+  end
 end
