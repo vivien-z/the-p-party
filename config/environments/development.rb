50c4c0a93ec5594708b296adfc5c9f7cd98c080b
vivien zhu <yuanzyy@gmail.com>
2021-02-23 13:26:06 -0500

    add style to signup and login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Development environment, defined default url options
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
