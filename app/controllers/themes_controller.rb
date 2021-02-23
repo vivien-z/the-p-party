3f6d236cb895b60806a156d860aaf92366236373
Marjorie Joseph <marjo_joseph86@hotmail.com>
2021-02-23 13:15:24 -0500

    finished the create/new theme

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,2 +1,30 @@
 class ThemesController < ApplicationController
+  def index
+    @themes = Theme.all
+  end
+
+  def show
+    @theme = Theme.find(params[:id])
+  end
+
+  def new
+    @theme = Theme.new
+  end
+
+  def create
+    @theme = Theme.new(theme_params)
+    @theme.user = current_user
+    if @theme.save
+      redirect_to theme_path(@theme)
+    else
+      render 'theme/show'
+    end
+  end
+
+  private
+
+  def theme_params
+    params.require(:theme).permit(:name, :description, :price_cent, :rating, :number_people)
+  end
 end
+
