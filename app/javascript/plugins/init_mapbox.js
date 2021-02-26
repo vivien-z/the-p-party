28bbafd09153fb0602abf0583b94ac8476db0cd8
romuald gaboriau <gaboriau.r@gmail.com>
2021-02-25 23:20:42 -0500

    Updated css for cards and changed the style of the map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,7 +14,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/rom1023/cklls66kq1s4317pcih4ervzl'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
