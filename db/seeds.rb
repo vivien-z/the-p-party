add59ed88554244a2670020e4f0abe7a7a2ed603
vivien zhu <yuanzyy@gmail.com>
2021-02-25 23:31:59 -0500

    adjust the price setting in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,31 +15,31 @@ romuald = User.create!(email: "romuald@h.com" , password: "123456")
 stephane = User.create!(email: "stephane@h.com" , password: "123456")
 
 file = URI.open("https://www.sandyalamode.com/wp-content/uploads/2017/09/SLPF-1-800x534.jpg")
-unicorn = Theme.new(name: "Pinky unicorn", address: "295 Rue de la Montagne, Montreal", description: "Fantasy, dreamy and pink cloud.", price_cent: 12000, number_people: 10)
+unicorn = Theme.new(name: "Pinky unicorn", address: "295 Rue de la Montagne, Montreal", description: "Fantasy, dreamy and pink cloud.", price_cent: 120, number_people: 10)
 unicorn.user = marjorie
 unicorn.photos.attach(io: file, filename: 'unicorn.jpg', content_type: 'image/jpg')
 unicorn.save!
 
 file = URI.open("https://links.imagerelay.com/cdn/34/ql/494f11d15454499bbd82a1521ef30766/Cheddar_Island_950x772_72_RGB.jpg")
-cheesy_mango = Theme.new(name: "Cheesy Mango", address: "150 Rue Saint-Hubert, Montreal", description: "Fan of drinking cheesy mango drink, at the beach?", price_cent: 15000, number_people: 10)
+cheesy_mango = Theme.new(name: "Cheesy Mango", address: "150 Rue Saint-Hubert, Montreal", description: "Fan of drinking cheesy mango drink, at the beach?", price_cent: 150, number_people: 10)
 cheesy_mango.user = stephane
 cheesy_mango.photos.attach(io: file, filename: 'mango.jpg', content_type: 'image/jpg')
 cheesy_mango.save!
 
 file = URI.open("https://karaspartyideas.com/wp-content/uploads/2019/08/Alice-in-Wonderland-Tea-Party-via-Karas-Party-Ideas-KarasPartyIdeas.com9_.jpg")
-alice = Theme.new(name: "Alice In Wonderland", address: "3220 avenue ridgewood montreal", description: "Magical and a running rabbit with his clock.", price_cent: 17000, number_people: 10)
+alice = Theme.new(name: "Alice In Wonderland", address: "3220 avenue ridgewood montreal", description: "Magical and a running rabbit with his clock.", price_cent: 170, number_people: 10)
 alice.user = elenice
 alice.photos.attach(io: file, filename: 'alice.jpg', content_type: 'image/jpg')
 alice.save!
 
 file = URI.open("https://s3.amazonaws.com/files.gofox.com/wp-content/uploads/2015/08/hurt-4.jpg")
-vikings = Theme.new(name: "Vikings", address: "493 Rue de Cherbourg, Longueuil", description: "Party hard like a Viking!", price_cent: 20000, number_people: 10)
+vikings = Theme.new(name: "Vikings", address: "493 Rue de Cherbourg, Longueuil", description: "Party hard like a Viking!", price_cent: 200, number_people: 10)
 vikings.user = romuald
 vikings.photos.attach(io: file, filename: 'vikings.jpg', content_type: 'image/jpg')
 vikings.save!
 
 file = URI.open("https://photos-cdn.catchmyparty.com/BL/2018/10/skull.jpg")
-halloween = Theme.new(name: "Halloween", address: "222 curé-poirier, Laval", description: "Don't want to wait on october 31th? No problem.", price_cent: 10000, number_people: 10)
+halloween = Theme.new(name: "Halloween", address: "222 curé-poirier, Laval", description: "Don't want to wait on october 31th? No problem.", price_cent: 100, number_people: 10)
 halloween.user = vivien
 halloween.photos.attach(io: file, filename: 'halloween.jpg', content_type: 'image/jpg')
 halloween.save!
